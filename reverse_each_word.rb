abd4ad3c258a3cbb29a259995705ea457efb63c6
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 18:33:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 def reverse_each_word(string)
   words = string.split(" ")
-  x = words.length - 1 
-  answer = ""
-  while x >= 0
-    answer += words[x]
-    x -= 1
+  words.each do |word|
+    word.collect 
   end
-  return answer
 end
 
